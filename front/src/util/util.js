62245569746dd57e090f48c73e3b35aa69f3f2e8
biggsky <sunghyun0624@naver.com>
2023-08-29 16:24:55 +0900

    edit

--- front/src/util/util.js
+++ front/src/util/util.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const ipUrl = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "http://hyunss.com/api",
+  baseURL: "https://hyunss.com/api",
   withCredentials: true,
 });
 export { ipUrl };
